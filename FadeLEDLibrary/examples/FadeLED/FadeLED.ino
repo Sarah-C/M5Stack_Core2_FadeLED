1bca1b605803cd4a092c0b7e0c7a08c3d4d7475c
Sarah Cartwright <sarah@untamed.zone>
2021-08-13 08:01:27 +0100

    Add files via upload

--- FadeLEDLibrary/examples/FadeLED/FadeLED.ino
+++ FadeLEDLibrary/examples/FadeLED/FadeLED.ino
@@ -1,14 +1,17 @@
 #include <M5Core2.h>
 #include "FadeLED.h"
 
-int stage = 0;
+uint8_t stage = 0;
+uint8_t oldStage = 1;
 
 void setup() {
   M5.begin();
   randomSeed(analogRead(0));
-  fade.brightness(2);
-  xTaskCreatePinnedToCore(updateDisplay, "Update display.", 1000, NULL, 1, NULL, 1);
+  fade.brightness(1);
+  xTaskCreatePinnedToCore(updateDisplayTask, "Update display.", 1000, NULL, 1, NULL, 1);
 }
+
+void updateDisplay(void);
 
 void loop(void) {
   M5.update();
@@ -44,6 +47,8 @@ void maybeAddNewFadyStage() {
     }
     // Red swish...
     if (stage == 1 || stage == 3) {
+      // M5.Lcd.fillScreen(TFT_BLACK);
+      //M5.Lcd.drawString("Red swish", 160, 145, 4);
       for (int i = 0; i < 10; i++) {
         addFade(255, 0, 0, 5 + (5 * i), 0, 3, 9 - i);
         addFade(255, 128, 128, 0, 0, 1, 9 - i);
@@ -55,6 +60,8 @@ void maybeAddNewFadyStage() {
     }
     // Multicoloured rug...
     if (stage == 4) {
+      //M5.Lcd.fillScreen(TFT_BLACK);
+      //M5.Lcd.drawString("Patchwork quilt", 160, 145, 4);
       for (int i = 0; i < 90; i++) {
         uint8_t led = random(0, 10);
         addFade(random(0, 255), random(0, 255), random(0, 255),    random(1, 20), 0,  random(5, 20), led);
@@ -64,6 +71,8 @@ void maybeAddNewFadyStage() {
       return;
     }
     // Camera flashes...
+    //M5.Lcd.fillScreen(TFT_BLACK);
+    //M5.Lcd.drawString("Camera flashes", 160, 145, 4);
     if (stage == 5) {
       for (int i = 0; i < 150; i++) {
         uint8_t led = random(0, 10);
@@ -74,7 +83,9 @@ void maybeAddNewFadyStage() {
       return;
     }
     // Police flashers...
-    if (stage < 15) {
+    if (stage >= 6 && stage < 15) {
+      // M5.Lcd.fillScreen(TFT_BLACK);
+      //M5.Lcd.drawString("Police flasher", 160, 145, 4);
       for (int led = 0; led < 5; led++) {
         addFade(255, 0, 0, 26, 0, 1, led);
         addFade(0, 0, 0, 0, 0, 1, led);
@@ -94,7 +105,9 @@ void maybeAddNewFadyStage() {
       return;
     }
     // Warning orange flasher
-    if (stage < 19) {
+    if (stage >= 15 && stage < 19) {
+      //  M5.Lcd.fillScreen(TFT_BLACK);
+      //M5.Lcd.drawString("Warning light", 160, 145, 4);
       for (int i = 0; i < 10; i++) {
         addFade(255, 128, 0,   0, 0,    100, i);
         addFade(0, 0, 0,       0, 25,   1,   i);
@@ -118,16 +131,29 @@ void maybeAddNewFadyStage() {
 //##########################################
 
 // Task that runs independently showing on the M5Stack Core2 screen what the LED's are displaying.
-void updateDisplay(void * parameter) {
+void updateDisplayTask(void * parameter) {
+  M5.Lcd.setTextDatum(MC_DATUM); // Middle center align
+  M5.Lcd.setTextColor(TFT_WHITE);
   // Configure with this.
   const uint16_t miniBoxSize = 25;
   const uint16_t miniBoxSizeLessBorder = miniBoxSize - 2;
   const uint16_t bigSquareWidth = 25;
   // Other calcs.
   const uint16_t reverseCenterHorizontalBar = miniBoxSize * 9;
-  const uint16_t centerHorizontalBar = 161 - ((10 * miniBoxSize) >> 1);
+  const uint16_t centerHorizontalBar = 160 - ((10 * miniBoxSize) >> 1);
   const uint16_t horBarOffets = centerHorizontalBar + reverseCenterHorizontalBar;
   for (;;) { // infinite loop
+    uint8_t s = stage - 1;
+    if (oldStage != s) {
+      M5.Lcd.fillRect(50, 135, 220, 32, 0);
+      oldStage = s;
+      if (s == 0 || s == 2) M5.Lcd.drawString("Green swish", 160, 150, 4);
+      if (s == 1 || s == 3) M5.Lcd.drawString("Red swish", 160, 150, 4);
+      if (s == 4) M5.Lcd.drawString("Patchwork quilt", 160, 150, 4);
+      if (s == 5) M5.Lcd.drawString("Camera flashes", 160, 150, 4);
+      if (s >= 6 && s < 15) M5.Lcd.drawString("Police flashes", 160, 150, 4);
+      if (s >= 15 && s < 19) M5.Lcd.drawString("Warning flashes", 160, 150, 4);
+    }
     for (uint8_t i = 0; i < 10; i++) {
       int y = (i < 5) ? i * 48 : 240 - ((i - 4) * 48);
       int x = (i < 5) ? (320 - bigSquareWidth) : 0;
