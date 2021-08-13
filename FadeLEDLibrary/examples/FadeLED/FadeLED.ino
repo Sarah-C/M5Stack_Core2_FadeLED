930e4f44aea8ed2e1777c46ba3db1987eb9718a4
Sarah Cartwright <sarah@untamed.zone>
2021-08-13 07:18:55 +0100

    Add files via upload

--- FadeLEDLibrary/examples/FadeLED/FadeLED.ino
+++ FadeLEDLibrary/examples/FadeLED/FadeLED.ino
@@ -6,7 +6,7 @@ int stage = 0;
 void setup() {
   M5.begin();
   randomSeed(analogRead(0));
-  fade.brightness(1);
+  fade.brightness(2);
   xTaskCreatePinnedToCore(updateDisplay, "Update display.", 1000, NULL, 1, NULL, 1);
 }
 
@@ -17,13 +17,15 @@ void loop(void) {
   maybeAddNewFadyStage();
 }
 
-void addFade(uint8_t r, uint8_t g, uint8_t b, uint16_t fadeCountdown, uint16_t transitionSteps, uint8_t led) {
+void addFade(uint8_t r, uint8_t g, uint8_t b, uint16_t fadeCountdown, uint16_t fadeCountup, uint16_t transitionSteps, uint8_t led) {
   FadeQueue fq;
   fq.startRGB = CRGB(0, 0, 0);
   fq.targetRGB = CRGB(r, g, b);
   fq.useStartColor = false;
+  if (transitionSteps == 0) transitionSteps = 1;
   fq.fadeTransitionSteps = transitionSteps;
   fq.fadeCountdown = fadeCountdown;
+  fq.fadeCountup = fadeCountup;
   fade.addFadeToQueue(fq, led);
 }
 
@@ -32,10 +34,10 @@ void maybeAddNewFadyStage() {
     // Green swish...
     if (stage == 0 || stage == 2) {
       for (int i = 0; i < 10; i++) {
-        addFade(0, 255, 0, 5 + (5 * i), 3, i);
-        addFade(128, 255, 128, 0, 1, i);
-        addFade(0, 255, 0, 0, 1, i);
-        addFade(0, 0, 0, 0, 15, i);
+        addFade(0, 255, 0, 5 + (5 * i), 0, 3, i);
+        addFade(128, 255, 128, 0, 0, 1, i);
+        addFade(0, 255, 0, 0, 0, 1, i);
+        addFade(0, 0, 0, 0, 0, 15, i);
       }
       stage++;
       return;
@@ -43,10 +45,10 @@ void maybeAddNewFadyStage() {
     // Red swish...
     if (stage == 1 || stage == 3) {
       for (int i = 0; i < 10; i++) {
-        addFade(255, 0, 0, 5 + (5 * i), 3, 9 - i);
-        addFade(255, 128, 128, 0, 1, 9 - i);
-        addFade(255, 0, 0, 0, 1, 9 - i);
-        addFade(0, 0, 0, 0, 15, 9 - i);
+        addFade(255, 0, 0, 5 + (5 * i), 0, 3, 9 - i);
+        addFade(255, 128, 128, 0, 0, 1, 9 - i);
+        addFade(255, 0, 0, 0, 0,  1, 9 - i);
+        addFade(0, 0, 0, 0, 0, 15, 9 - i);
       }
       stage++;
       return;
@@ -55,8 +57,8 @@ void maybeAddNewFadyStage() {
     if (stage == 4) {
       for (int i = 0; i < 90; i++) {
         uint8_t led = random(0, 10);
-        addFade(random(0, 255), random(0, 255), random(0, 255),    random(1, 20),   random(5, 20), led);
-        addFade(150, 0, 0, 0, 10, led);
+        addFade(random(0, 255), random(0, 255), random(0, 255),    random(1, 20), 0,  random(5, 20), led);
+        addFade(150, 0, 0, 0, 0, 10, led);
       }
       stage++;
       return;
@@ -65,8 +67,8 @@ void maybeAddNewFadyStage() {
     if (stage == 5) {
       for (int i = 0; i < 150; i++) {
         uint8_t led = random(0, 10);
-        addFade(255, 255, 255, random(1, 50), 1, led);
-        addFade(0, 0, 0, 0, 1, led);
+        addFade(255, 255, 255, random(1, 50), 0, 1, led);
+        addFade(0, 0, 0, 0, 0, 1, led);
       }
       stage ++;
       return;
@@ -74,42 +76,67 @@ void maybeAddNewFadyStage() {
     // Police flashers...
     if (stage < 15) {
       for (int led = 0; led < 5; led++) {
-        addFade(255, 0, 0, 26, 1, led);
-        addFade(0, 0, 0, 0, 1, led);
-        addFade(255, 0, 0, 7, 1, led);
-        addFade(0, 0, 0, 0, 1, led);
+        addFade(255, 0, 0, 26, 0, 1, led);
+        addFade(0, 0, 0, 0, 0, 1, led);
+        addFade(255, 0, 0, 7, 0, 1, led);
+        addFade(0, 0, 0, 0, 0, 1, led);
       }
       for (int led = 5; led < 10; led++) {
-        addFade(0, 0, 0, 13, 1, led);
+        addFade(0, 0, 0, 13, 0, 1, led);
       }
       for (int led = 5; led < 10; led++) {
-        addFade(0, 0, 255, 26, 1, led);
-        addFade(0, 0, 0, 0, 1, led);
-        addFade(0, 0, 255, 7, 1, led);
-        addFade(0, 0, 0, 0, 1, led);
+        addFade(0, 0, 255, 26, 0, 1, led);
+        addFade(0, 0, 0, 0, 0, 1, led);
+        addFade(0, 0, 255, 7, 0, 1, led);
+        addFade(0, 0, 0, 0, 0, 1, led);
       }
       stage ++;
+      return;
+    }
+    // Warning orange flasher
+    if (stage < 19) {
+      for (int i = 0; i < 10; i++) {
+        addFade(255, 128, 0,   0, 0,    100, i);
+        addFade(0, 0, 0,       0, 25,   1,   i);
+        addFade(255, 128, 0,   0, 25,   1,   i);
+        addFade(0, 0, 0,       0, 25,   1,   i);
+        addFade(255, 128, 0,   0, 25,   1,   i);
+        addFade(0, 0, 0,       0, 25,   1,   i);
+        addFade(255, 128, 0,   0, 25,   1,   i);
+        addFade(0, 0, 0,       0, 25,   1,   i);
+        addFade(255, 128, 0,   0, 25,   1,   i);
+        addFade(0, 0, 0,       0, 0,    100, i);
+      }
+      stage++;
       return;
     }
     stage = 0;
   }
 }
+
+
 //##########################################
 
 // Task that runs independently showing on the M5Stack Core2 screen what the LED's are displaying.
 void updateDisplay(void * parameter) {
-  const uint16_t size = 15;
-  const uint16_t xo = 160 - ((10 * size) >> 1);
+  // Configure with this.
+  const uint16_t miniBoxSize = 25;
+  const uint16_t miniBoxSizeLessBorder = miniBoxSize - 2;
+  const uint16_t bigSquareWidth = 25;
+  // Other calcs.
+  const uint16_t reverseCenterHorizontalBar = miniBoxSize * 9;
+  const uint16_t centerHorizontalBar = 161 - ((10 * miniBoxSize) >> 1);
+  const uint16_t horBarOffets = centerHorizontalBar + reverseCenterHorizontalBar;
   for (;;) { // infinite loop
     for (uint8_t i = 0; i < 10; i++) {
       int y = (i < 5) ? i * 48 : 240 - ((i - 4) * 48);
-      int x = (i < 5) ? (320 - 50) : 0;
+      int x = (i < 5) ? (320 - bigSquareWidth) : 0;
       uint16_t r = round(fade.fadeLEDs[i].currentRGB.r);
       uint16_t g = round(fade.fadeLEDs[i].currentRGB.g);
       uint16_t b = round(fade.fadeLEDs[i].currentRGB.b);
       uint16_t col = ((r & 0b11111000) << 8) | ((g & 0b11111100) << 3) | (b >> 3);
-      M5.Lcd.fillRect(x, y, 50, 48, col);
-      M5.Lcd.fillRect(xo + (i * size), 90, size - 2, size - 2, col);
+      M5.Lcd.fillRect(x, y, bigSquareWidth, 48, col);
+      M5.Lcd.fillRect(horBarOffets - (i * miniBoxSize), 90, miniBoxSizeLessBorder, miniBoxSizeLessBorder, col);
     }
     // Pause the task
     vTaskDelay(10 / portTICK_PERIOD_MS);
